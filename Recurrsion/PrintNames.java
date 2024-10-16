261f62770ff4500ba862699c12a25df47b53f3bc
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2024-10-16 14:45:11 +0530

    recursion added

---
+++ Recurrsion/PrintNames.java
@@ -0,0 +1,16 @@
+package Recurrsion;
+
+public class PrintNames {
+     public static void printName(int start, int n, String name) {
+          if (start > n) {
+               return;
+          }
+          System.out.println(name);
+          printName(start + 1, n, name);
+     }
+
+     public static void main(String[] args) {
+          printName(1, 5, "Nithish");
+     }
+
+}
